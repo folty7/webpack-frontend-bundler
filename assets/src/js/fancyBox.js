351c48564e431c91ece6314b8bf0988e32785597
afolta <ondrej4a@gmail.com>
2026-01-02 20:19:06 +0100

    first commit

--- assets/src/js/fancyBox.js
+++
@@ -1,6 +0,0 @@
-import { Fancybox } from '@fancyapps/ui';
-import 'node_modules/@fancyapps/ui/dist/fancybox/fancybox.css';
-
-Fancybox.bind('[data-fancybox="gallery"]', {
-    //
-});
--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -1,23 +1,4 @@
 import joke from './index2.js';
-import '../scss/main.scss';
-
-// import Atropos library
-import Atropos from 'atropos';
-
-const myAtropos = Atropos({
-    el: '.my-atropos',
-    activeOffset: 40,
-    shadowScale: 1.05,
-    onEnter() {
-        console.log('Enter');
-    },
-    onLeave() {
-        console.log('Leave');
-    },
-    onRotate(x, y) {
-        console.log('Rotate', x, y);
-    }
-});
 
 // Example ES6 code
 const greeting = () => {
